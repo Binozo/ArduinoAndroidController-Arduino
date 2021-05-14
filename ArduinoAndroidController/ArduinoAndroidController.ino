a932433cba66e070cfbb39bc58570898d3bb4195
Binozo <noah.czaplewski05@gmail.com>
2021-05-14 19:44:33 +0200

    Arduino Wifi Code verbessert. Der Arduino ist ab jetzt der Access Point

--- ArduinoAndroidController/ArduinoAndroidController.ino
+++ ArduinoAndroidController/ArduinoAndroidController.ino
@@ -2,56 +2,132 @@
 #include <WiFiNINA.h>
 
 
-char ssid[] = "ssid";        // your network SSID (name)
-char pass[] = "passwort";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "Ardino";        // your network SSID (name)
+char pass[] = "binozoworks";    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
+WiFiServer server(8140);
 
 void setup() {
-  //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial); //Warte bis der Serial Port verbunden ist
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
+  Serial.println("Boote TCP System...");
 
-    // wait 10 seconds for connection:
-    delay(10000);
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Booten fehlgeschlagen: Kommunikation mit WiFi Modul fehlgeschlagen");
+    // don't continue
+    while (true);
   }
 
-  // you're connected now, so print out the data:
-  Serial.println("You're connected to the network");
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Info: Es gibt ein neues Firmware upgrade");
+  }
+  
+  // print the network name (SSID);
+  Serial.print("Es wurde ein Access Point auf Port 8140 erstellt: ");
+  Serial.println(ssid);
 
-  Serial.println("----------------------------------------");
-  printData();
-  Serial.println("----------------------------------------");
+  // Create open network. Change this line if you want to create an WEP network:
+  status = WiFi.beginAP(ssid, pass);
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Booten fehlgeschlagen: Creating access point failed");
+    // don't continue
+    while (true);
+  }
+
+  // start the web server on port 80
+  server.begin();
+
+  // you're connected now, so print out the status
+  printWiFiStatus();
 }
 
 void loop() {
-  // check the network connection once every 10 seconds:
- delay(10000);
- printData();
- Serial.println("----------------------------------------");
+  // compare the previous status to the current status
+  if(status != WiFi.status()) {
+    // it has changed update the variable
+    status = WiFi.status();
+
+    if(status == WL_AP_CONNECTED) {
+      // a device has connected to the AP
+      Serial.println("Device connected to AP");
+    } else {
+      // a device has disconnected from the AP, and we are back in listening mode
+      Serial.println("Device disconnected from AP");
+    }
+  }
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+
+            int randomReading = analogRead(A1);
+            client.print("Random reading from analog pin: ");
+            client.print(randomReading);
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(led, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(led, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
 }
 
-void printData() {
-  Serial.println("Board Information:");
-  // print your board's IP address:
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  Serial.println();
-  Serial.println("Network Information:");
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 
 }
